abbd8786739a1ae1faf7d76d6bc80aa9f9198201
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-18 11:06:34 +0200

    TicketLottery: first commit

---
+++ TicketLottery.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class TicketLottery {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(getProbabilities(sc.nextLine()));
+    }
+
+    public static String getProbabilities(String input) {
+        return "";
+    }
+}
