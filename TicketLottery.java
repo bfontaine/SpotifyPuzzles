d088dabeb6a602ee444d4bcec99885b85ec87e39
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-18 11:18:31 +0200

    return '0' if not enough tickets for the group

--- TicketLottery.java
+++ TicketLottery.java
@@ -2,13 +2,37 @@ import java.util.Scanner;
 
 class TicketLottery {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println(getProbabilities(sc.nextLine()));
     }
 
     public static String getProbabilities(String input) {
+
+        int[] numbers = splitNumbers(input);
+
+        if (!enoughTickets(numbers))
+            return "0";
+
         return "";
+    }
+
+    private static int[] splitNumbers(String input) {
+        String[] parts = input.split(" ");
+
+        return new int[] {
+            Integer.parseInt(parts[0]),
+            Integer.parseInt(parts[1]),
+            Integer.parseInt(parts[2]),
+            Integer.parseInt(parts[3])
+        };
+    }
+
+    // test if it’s possible to win enough tickets
+    // for the whole group
+    private static boolean enoughTickets(int[] numbers) {
+        // number of winners * number of tickets per winner >= |group|
+        return (numbers[1]*numbers[2] >= numbers[3]);
     }
 }
