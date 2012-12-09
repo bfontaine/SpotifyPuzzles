2b958be376458dba8041939439abbec8b6d3361d
Baptiste Fontaine <batifon@yahoo.fr>
2012-12-09 19:55:49 +0100

    Junit tests are not run if the Java files don't compile

--- junit.sh
+++ junit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # tool to test a .java file with JUnit
 
-javac -cp .:/usr/share/java/junit4.jar $1
+javac -cp .:/usr/share/java/junit4.jar $1 && \
 java -cp .:/usr/share/java/junit4.jar org.junit.runner.JUnitCore ${1%%.java}
